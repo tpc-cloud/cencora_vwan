5e841b7160d9a4a00b74f54c21b095cb1018dec5
Kenneth Hopkins <kenneth.hopkins@virtustream.com>
2025-06-17 22:46:10 -0400

    test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,13 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.85.0"
+    }
+    random = {
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
