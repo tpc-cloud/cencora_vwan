815b420f230bce7a7c12aae39e8d7635288e1ad9
Kenneth Hopkins <kenneth.hopkins@virtustream.com>
2025-06-17 22:50:42 -0400

    test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  use_oidc        = true
 }
 
 resource "azurerm_resource_group" "vwan" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,31 @@
+variable "client_id" {
+  description = "The Client ID for the Service Principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Service Principal"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for the Service Principal"
+  type        = string
+}
+
 variable "environment" {
-  description = "Environment name (e.g., dev, prod)"
+  description = "The environment (e.g., dev, prod)"
   type        = string
 }
 
 variable "location" {
-  description = "Azure region where resources will be created"
+  description = "The Azure region to deploy resources"
   type        = string
   default     = "eastus"
 }
 
 variable "tags" {
-  description = "Tags to be applied to all resources"
+  description = "Tags to apply to all resources"
   type        = map(string)
   default     = {}
 }
