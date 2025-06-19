c27d888128faee843a38b2f3a26ebb5ebc7263fb
Kenneth Hopkins <kenneth.hopkins@virtustream.com>
2025-06-18 21:07:57 -0400

    new tactic

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,10 @@ terraform {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "stterraformstatedev"
     container_name       = "tfstate"
-    key                  = "vwan.tfstate"
+    key                  = "prod/hub1.tfstate"
+    use_oidc            = true
   }
-} 
+}
+
+# Note: This file serves as a template and will be dynamically updated by the GitHub Actions workflow
+# The actual values for storage_account_name and key will be set during the workflow execution 
