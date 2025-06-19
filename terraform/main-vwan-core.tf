3a49b85d055940f8caf327ef2a8fadb624fce106
Kenneth Hopkins <kenneth.hopkins@virtustream.com>
2025-06-19 17:04:54 -0400

    new try

--- terraform/main-vwan-core.tf
+++ terraform/main-vwan-core.tf
@@ -18,14 +18,17 @@ provider "azurerm" {
   use_oidc = true
 }
 
-data "azurerm_resource_group" "vwan" {
-  name = "rg-vwan-${var.environment}"
+# Create the resource group for Virtual WAN
+resource "azurerm_resource_group" "vwan" {
+  name     = "rg-vwan-${var.environment}"
+  location = var.location
+  tags     = var.tags
 }
 
 # Create the Virtual WAN without any hubs
 resource "azurerm_virtual_wan" "vwan" {
   name                = "vwan-${var.environment}"
-  resource_group_name = data.azurerm_resource_group.vwan.name
+  resource_group_name = azurerm_resource_group.vwan.name
   location            = var.location
   type                = "Standard"
   tags                = var.tags
